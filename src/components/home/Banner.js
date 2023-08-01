2da8c4fb66a891497177d1b1e053ae983d12d60e
HadiKia <hadikiamarzi@protonmain.com>
2023-08-01 23:38:29 +0330

    Banner updated

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -6,21 +6,21 @@ import { Pagination, Autoplay } from "swiper/modules";
 import 'swiper/css/autoplay'
 
 // images
-import slider from '../../images/slider.png'
-import slider2 from '../../images/slider2.png'
-import slider3 from '../../images/slider3.png'
+import slider from '../../images/slider.svg'
+import slider2 from '../../images/slider2.svg'
+import slider3 from '../../images/slider3.svg'
 
 // styles
-const swiperStyle = 'w-full h-[11em]'
-const swiperSlideStyle = 'text-center py-[4.813em] h-[11em] bg-[image:var(--image-url)] bg-cover bg-center'
-const pStyle = 'text-[#E5F2E9] font-bold'
-const buttonStyle = 'bg-[#417F56] py-1.5 px-3 text-[10px] font-medium text-white rounded-md mt-3.5'
+const swiperStyle = 'w-full h-[11em] md:h-[21em]'
+const swiperSlideStyle = 'text-center py-[4.813em] h-[11em] bg-[image:var(--image-url)] bg-cover bg-center md:py-[7.75em] md:h-[21em]'
+const pStyle = 'text-[#E5F2E9] font-bold md:text-3xl lg:text-4xl'
+const buttonStyle = 'bg-[#417F56] py-1.5 px-3 text-[10px] font-medium text-white rounded-md mt-3.5 md:mt-12 xl:mt-16 md:py-2 md:px-6 md:text-base '
 
 const Banner = () => {
  
   return (
     <>
-      <Swiper  pagination={true} modules={[Pagination, Autoplay]} autoplay={true} className={swiperStyle}>
+      <Swiper pagination={true} modules={[Pagination, Autoplay]} autoplay={{delay: 4000}} className={swiperStyle}>
         <SwiperSlide style={{ "--image-url": `url(${slider})` }} className={swiperSlideStyle}>
           <p className={pStyle}>تجربه غذای سالم و گیاهی به سبک ترخینه</p>
           <button className={buttonStyle}>سفارش آنلاین غذا</button>
@@ -32,8 +32,7 @@ const Banner = () => {
         <SwiperSlide style={{ "--image-url": `url(${slider3})` }} className={swiperSlideStyle}>
           <p className={pStyle}>لذت غذای سالم و گیاهی را با ترخینه تجربه کنید!</p>
           <button className={buttonStyle}>سفارش آنلاین غذا</button>
-        </SwiperSlide>
-        
+        </SwiperSlide> 
       </Swiper>
     </>
   );
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Banner from "./Banner";
-import BannerDesktop from "./BannerDesktop";
 import HomePageMenu from "./HomePageMenu";
 
 const HomePage = () => {
   return (
     <>
-      <div className="md:hidden"><Banner /></div>
-      <div className="hidden md:block"><BannerDesktop /></div>
+      <Banner />
       <HomePageMenu />
     </>
   );
