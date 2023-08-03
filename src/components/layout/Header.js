6b8da55db0bc6406785a96c305ee39c38dfec066
HadiKia <hadikiamarzi@protonmain.com>
2023-08-03 10:54:21 +0330

    Header style updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { RadioGroup } from "@headlessui/react";
 import MobileMenu from "./MobileMenu";
 
 // icons
@@ -15,14 +16,17 @@ import {
 // styles
 const headerStyle =
   "container max-w-[1224px] mx-auto flex justify-between items-center h-[4em] md:h-[7.188em] px-[1.25em]";
-const ulStyle = "flex items-center gap-x-[1.5em]";
+const ulStyle = "flex items-center md:gap-x-5 lg:gap-x-6";
 const liStyle =
-  "text-[#717171] text-sm lg:text-lg hover:font-bold border-b py-1 border-[#ffffff00] hover:text-[#417F56] hover:border-[#417F56] duration-500 cursor-pointer";
+  "text-[#717171] text-sm lg:text-lg border-b border-[#ffffff00] py-1 duration-500";
+const liActiveStyle = '!text-[#417F56] text-sm lg:text-lg border-b border-[#417F56] py-1 font-bold duration-500 '
 const buttonBoxStyle =
   "flex items-center gap-x-[0.25em] md:gap-x-[0.5em] justify-end";
 const headerButtonStyle = "p-[0.25em] md:p-[0.5em] bg-[#E5F2E9] rounded-md";
 
 const Header = () => {
+  let [plan, setPlan] = useState("home");
+
   return (
     <div className="sticky top-0 z-10 bg-white">
       <header className={headerStyle}>
@@ -32,18 +36,44 @@ const Header = () => {
 
         <div>
           <span className="md:hidden">{logo}</span>
-          <span className="hidden md:block">{logoDesktop}</span>
+          <span className="hidden md:block md:scale-[.8] lg:scale-100">{logoDesktop}</span>
         </div>
 
         <div className="hidden md:block">
-          <ul className={ulStyle}>
-            <li className={liStyle}>صفحه اصلی</li>
-            <li className={liStyle}>شعبه</li>
-            <li className={liStyle}>منو</li>
-            <li className={liStyle}>اعطای نمایندگی</li>
-            <li className={liStyle}>درباره ما</li>
-            <li className={liStyle}>تماس با ما</li>
-          </ul>
+          <RadioGroup value={plan} onChange={setPlan}>
+            <ul className={ulStyle}>
+              <RadioGroup.Option value="home">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle} ` : `${liStyle}` } >صفحه اصلی</li>
+                )}
+              </RadioGroup.Option>
+              <RadioGroup.Option value="branch">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle}` : `${liStyle}` } >شعبه</li>
+                )}
+              </RadioGroup.Option>
+              <RadioGroup.Option value="menu">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle}` : `${liStyle}` } >منو</li>
+                )}
+              </RadioGroup.Option>
+              <RadioGroup.Option value="franchise">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle}` : `${liStyle}` } >اعطای نمایندگی</li>
+                )}
+              </RadioGroup.Option>
+              <RadioGroup.Option value="aboutus">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle}` : `${liStyle}` } >درباره ما</li>
+                )}
+              </RadioGroup.Option>
+              <RadioGroup.Option value="calling">
+                {({ checked }) => (
+                  <li className={ checked ? `${liActiveStyle}` : `${liStyle}` } >تماس با ما</li>
+                )}
+              </RadioGroup.Option>
+            </ul>
+          </RadioGroup>
         </div>
 
         <div className={buttonBoxStyle}>
