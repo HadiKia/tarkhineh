9d4ad138f5c5276e5fe68f7fc38ec959b7eb32f6
HadiKia <hadikiamarzi@protonmail.com>
2023-09-28 03:00:53 +0330

    MobileMenu styles updated

--- src/components/layout/MobileMenu.js
+++ src/components/layout/MobileMenu.js
@@ -20,13 +20,13 @@ import { Link } from "react-router-dom";
 // styles
 const bgMenu =
   "absolute top-0 left-0 right-0 h-screen z-10 bg-black bg-opacity-60 backdrop-blur-sm";
-const mobileMenu = "absolute top-0 right-0 z-10 w-[16em] duration-700";
-const closeIconStyle = "absolute top-[18px] right-[13.5em] text-white";
-const logoMobileStyle = "absolute right-4 top-8";
+const mobileMenu = "absolute top-0 right-0 z-10 w-[12em] sm:w-[16em] duration-700";
+const closeIconStyle = "absolute top-[18px] right-[9.5em] sm:right-[13.5em] text-white";
+const logoMobileStyle = "absolute right-4 top-6 sm:top-8";
 const ulStyle =
-  "text-right flex flex-col gap-y-2 min-h-[calc(100vh_-_94px)] bg-white w-[16em] px-4 py-2";
+  "text-right flex flex-col gap-y-2 min-h-[calc(100vh_-_70px)] sm:min-h-[calc(100vh_-_94px)] bg-white w-[12em] sm:w-[16em] px-4 py-2";
 const liStyle =
-  "flex items-center gap-x-2 text-[#353535] text-sm border-b border-[#CBCBCB] pb-2";
+  "flex items-center gap-x-2 text-[#353535] text-xs sm:text-sm border-b border-[#CBCBCB] pb-2";
 
 const MobileMenu = () => {
   const open = true;
