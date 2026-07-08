202adb95e5b9069ce30219feeb7ca63bccf078d4
HadiKia <hadikiamarzi@protonmail.com>
2026-07-09 02:05:02 +0330

    fix: prevent textarea from overflowing modal with long content

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-sm lg:rounded-lg border border-input hover:border-primary bg-transparent px-4 py-1.75  text-xs lg:text-base transition-all duration-300 ease-linear outline-none placeholder:text-gray-6 focus-visible:border-ring focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 resize-none text-gray-8",
+        "flex  min-h-16 w-full rounded-sm lg:rounded-lg border border-input hover:border-primary bg-transparent px-4 py-1.75  text-xs lg:text-base transition-all duration-300 ease-linear outline-none placeholder:text-gray-6 focus-visible:border-ring focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 resize-none text-gray-8",
         className,
       )}
       {...props}
