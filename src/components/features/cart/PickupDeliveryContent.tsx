cb0ff6d0d6b18460a63e88486f0a7b2c218915ef
HadiKia <hadikiamarzi@protonmail.com>
2026-08-23 01:44:07 +0330

    feat: display branch details for pickup delivery

--- src/components/features/cart/PickupDeliveryContent.tsx
+++ src/components/features/cart/PickupDeliveryContent.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { useBranchContext } from "@/contexts/BranchContext";
+import { Location } from "iconsax-reactjs";
+
 export default function PickupDeliveryContent() {
-  return <p>content</p>;
+  const { selectedBranch } = useBranchContext();
+  const { title, address, phoneNumber1, phoneNumber2, workingHours } =
+    selectedBranch ?? {};
+
+  return (
+    <section
+      aria-label="اطلاعات تحویل حضوری"
+      className="rounded-lg border border-gray-4 p-4 lg:px-6 lg:py-4"
+    >
+      <h3 className="flex items-center gap-1 pb-2 border-b border-gray-4 mb-4 lg:mb-3 text-sm lg:text-base text-gray-8">
+        <Location className="size-4 lg:size-6" />
+        {title}
+      </h3>
+
+      <div className="flex flex-col gap-2 text-xs lg:text-sm text-gray-7 px-3 lg:px-0">
+        <p>{address}</p>
+
+        <div className="flex flex-col">
+          <div className="flex items-center flex-wrap gap-x-2">
+            <span>شماره تماس ۱:</span>
+            <span>{phoneNumber1}</span>
+          </div>
+          {phoneNumber2 && (
+            <div className="flex items-center flex-wrap gap-x-2">
+              <span>شماره تماس ۲:</span>
+              <span>{phoneNumber2}</span>
+            </div>
+          )}
+        </div>
+
+        <div className="flex items-center flex-wrap gap-x-2">
+          <span>ساعت کاری: </span>
+          <span>{workingHours}</span>
+        </div>
+      </div>
+    </section>
+  );
 }
