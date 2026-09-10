9508a2f1ee1c37a9a55612d7eaa39ee398492de0
HadiKia <hadikiamarzi@protonmail.com>
2026-09-11 00:43:06 +0330

    feat: add in-person purchase notice content

--- src/components/features/cart/InPersonPurchaseContent.tsx
+++ src/components/features/cart/InPersonPurchaseContent.tsx
@@ -1,3 +1,15 @@
+import { Warning2 } from "iconsax-reactjs";
+import CartSectionTitle from "./CartSectionTitle";
+
 export default function InPersonPurchaseContent() {
-  return <p>inPerson</p>;
+  return (
+    <div className="rounded-lg border border-gray-4 p-4 lg:px-6 lg:py-7 flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between lg:gap-11 bg-gray-1 ">
+      <CartSectionTitle icon={Warning2}>قابل توجه</CartSectionTitle>
+      <p className="text-xs text-gray-7">
+        هزینه سفارش شما در حین تحویل کالا دریافت خواهد شد. لطفا قبل از تحویل
+        کالا کارت بانکی یا پول نقد همراه خود داشته باشید و از درخواست برای
+        پرداخت در زمان بعدی یا نسیه خودداری فرمایید. با تشکر از همراهی شما.
+      </p>
+    </div>
+  );
 }
